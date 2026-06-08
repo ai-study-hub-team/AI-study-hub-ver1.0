da61071cf8d55779647de2a509d80b5c77f55933
Trungnn01 <nhattrung16052005@gmail.com>
2026-06-09 01:03:59 +0700

    Remove unused files

--- frontend/src/app/components/RootLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router";
-
-export function RootLayout() {
-  return (
-    <div className="min-h-screen bg-slate-50 font-sans text-slate-900">
-      <Outlet />
-    </div>
-  );
-}
